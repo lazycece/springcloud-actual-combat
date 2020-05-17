f5e9b1b802a32a150e068e2496d6df4817480eee
lazycece <lazycece@gmail.com>
2020-05-17 22:38:28 +0800

    add dependency

---
+++ springcloud-ac-alibaba/springcloud-ac-alibaba-nacos-config/src/main/java/com/lazycece/scac/alibaba/nacos/config/NacosConfigController.java
@@ -0,0 +1,14 @@
+package com.lazycece.scac.alibaba.nacos.config;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lazycece
+ */
+@RestController
+@RefreshScope
+public class NacosConfigController {
+
+
+}
