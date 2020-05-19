0df097a06132f8ffbfc3c9fd000e2eafc96dc0aa
lazycece <lazycece@gmail.com>
2020-05-19 15:48:20 +0800

    zookeeper init

---
+++ springcloud-ac-zookeeper/src/main/java/com/lazycece/scac/zookeeper/SpringcloudAcZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAcZookeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAcZookeeperApplication.class, args);
+	}
+
+}
---
+++ springcloud-ac-zookeeper/src/test/java/com/lazycece/scac/zookeeper/SpringcloudAcZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAcZookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
