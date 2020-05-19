e16bdf5fe3e5315372aba65a3d0d911fd3a7efae
lazycece <lazycece@gmail.com>
2020-05-19 15:45:46 +0800

    netflix init

---
+++ springcloud-ac-netflix/src/main/java/com/lazycece/scac/netflix/SpringcloudAcNetflixApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.netflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAcNetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAcNetflixApplication.class, args);
+	}
+
+}
---
+++ springcloud-ac-netflix/src/test/java/com/lazycece/scac/netflix/SpringcloudAcNetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.netflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAcNetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
