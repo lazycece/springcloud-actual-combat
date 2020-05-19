92b4c493fba848d3c61a19741b44f6edbbbf48aa
lazycece <lazycece@gmail.com>
2020-05-19 16:04:00 +0800

    seata

---
+++ springcloud-ac-alibaba/springcloud-ac-alibaba-seata/src/main/java/com/lazycece/scac/alibaba/seata/SpringcloudAcAlibabaSeataApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.alibaba.seata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAcAlibabaSeataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAcAlibabaSeataApplication.class, args);
+	}
+
+}
---
+++ springcloud-ac-alibaba/springcloud-ac-alibaba-seata/src/test/java/com/lazycece/scac/alibaba/seata/SpringcloudAcAlibabaSeataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.alibaba.seata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAcAlibabaSeataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
