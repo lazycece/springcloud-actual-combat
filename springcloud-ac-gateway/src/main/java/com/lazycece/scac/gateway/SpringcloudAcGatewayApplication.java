19d7435432b5bd8606720dd66eb4f1867b6bea33
lazycece <lazycece@gmail.com>
2020-05-19 15:36:56 +0800

    gateway init

---
+++ springcloud-ac-gateway/src/main/java/com/lazycece/scac/gateway/SpringcloudAcGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.lazycece.scac.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAcGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudAcGatewayApplication.class, args);
+	}
+
+}
